0dca08782a1abb9f9de27ab0832dd99505cb3811
NorielSylvire <fl4v1u5@yandex.com>
2024-02-17 12:27:28 +0100

    Created initial project setup

--- 2nd/src/include/client.h
+++ 2nd/src/include/client.h
@@ -1,13 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   2ndTestClient.c                                    :+:      :+:    :+:   */
+/*   client.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fhongu <fhongu@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/02 20:30:12 by fhongu            #+#    #+#             */
-/*   Updated: 2024/02/03 01:54:40 by fhongu           ###   ########.fr       */
+/*   Created: 2024/02/17 12:18:03 by fhongu            #+#    #+#             */
+/*   Updated: 2024/02/17 12:21:07 by fhongu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLIENT_H
+# define CLIENT_H
+# include "minitalk.h"
 
+void	send_byte(int byte);
+
+#endif // !CLIENT_H
